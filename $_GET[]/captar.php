5c27280addd6fa33c8e0e48d9a0b361d86924a72
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2020-11-10 17:19:15 +0000

    Update captar.php, captar2.php, and 6 more files...

---
+++ $_GET[]/captar.php
@@ -0,0 +1,9 @@
+<?php
+f(isset($_GET['txt_nome']) && isset($_GET['txt_idade']) ){
+$nome = $_GET["txt_nome"];
+$idade = $_GET["txt_idade"];
+
+echo "Nome:" .$nome. "<br>";
+echo "Idade:" .$idade;
+}
+?>
